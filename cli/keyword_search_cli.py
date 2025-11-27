bb9ddcc726c2dd7969666499ed628514e9a6ff24
ayush.jaiswal1@squareyards.co.in <ayush.jaiswal1@squareyards.co.in>
2025-11-27 18:57:13 +0530

    changes

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
-from helper import search
+from helper import search_command
+
+from inverted_index import InvertedIndex
 
 
 def show_matched_movies(word):
-    matched_movies_title = search(word)
-    
+    matched_movies_title = search_command(word)
+
     for idx, title in enumerate(matched_movies_title):
         print(idx + 1, title, sep=". ")
-    
+
+
+def build_index():
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+
+    # docs = idx.get_documents("merida")
+    # print(f"First document for token 'merida' = {docs[0]}")
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -18,15 +29,21 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    # Add this line for the build command
+    build_parser = subparsers.add_parser("build", help="Build and save inverted index")
+
     args = parser.parse_args()
 
     match args.command:
         case "search":
             # print the search query here
             print("Searching for:", args.query)
-            
+
             word = args.query
             show_matched_movies(word)
+        case "build":
+            build_index()
+
         case _:
             parser.print_help()
 
